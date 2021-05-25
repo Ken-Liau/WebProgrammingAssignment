54b589a37c78aa7a4fb0dd46c4f7a1d71bbe7202
Ken329 <ken_037729@hotmail.com>
2021-05-25 16:07:56 +0800

    new changes

---
+++ partial/footer.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo '
+    <footer>
+    <h2>X Titan © 2021</h2>
+    <div>
+        <h4>Contact Us</h4>
+        <div class="contact-div">
+            <i class="fas fa-envelope"></i>
+            <i class="fas fa-phone"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+        </div>
+    </div>
+    </footer>
+    '
+
+?>
---
+++ partial/header.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '
+    <nav class="navbar">
+    <div class="brand-title">X Titan E-Comm</div>
+    <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </a>
+    <div class="navbar-links">
+        <ul>
+            <li><a href="#">Promotions</a></li>
+            <li><a href="#">Discounts</a></li>
+            <li><a href="#">Events</a></li>
+        </ul>
+    </div>
+    </nav>'
+
+?>
