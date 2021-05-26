6b47d15c542e08bbd588800bf750164faa100966
Ken329 <ken_037729@hotmail.com>
2021-05-26 20:38:37 +0800

    done anouncement page design and element page design

--- index.php
+++ index.php
@@ -11,95 +11,22 @@
 </head>
 <body>
     <?php
-        include_once('partial/header.php');
-    ?>
-    <div class="content-container">
-        <div class="content-title">
-            <h4>Promotions</h4>
-            <div class="content-title-div">
-                <button>See More</button>
-            </div>
-        </div>
-        <div class="content-con-div">
-            <div class="content-con">
-                <img src="img/promotion.jpg" alt="" srcset="">
-                <div class="content-detail">
-                    <p>Product Name</p>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired">
-                    Expired Date: 11/22/2020
-                </div>
-            </div>
-            <div class="content-con">
-                <img src="img/promotion.jpg" alt="" srcset="">
-                <div class="content-detail">
-                    <p>Product Name</p>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired">
-                    Expired Date: 11/22/2020
-                </div>
-            </div>
-            <div class="content-con">
-                <img src="img/promotion.jpg" alt="" srcset="">
-                <div class="content-detail">
-                    <p>Product Name</p>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired">
-                    Expired Date: 11/22/2020
-                </div>
-            </div>
-            <div class="content-con">
-                <img src="img/promotion.jpg" alt="" srcset="">
-                <div class="content-detail">
-                    <p>Product Name</p>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired">
-                    Expired Date: 11/22/2020
-                </div>
-            </div>
-        </div>
-        <div class="content-title">
-            <h4>Special Discount</h4>
-            <div class="content-title-div">
-                <button>See More</button>
-            </div>
-        </div>
-        <div class="content-con-div">
-            <div class="content-con">
-                <img src="img/special-discount.jpg" alt="" srcset="">
-                <div class="content-detail">
-                    <p>Product Name</p>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired">
-                    5:00PM to 7:00PM
-                </div>
-            </div>
-        </div>
-        <div class="content-title">
-            <h4>New Events</h4>
-            <div class="content-title-div">
-                <button>See More</button>
-            </div>
-        </div>
-        <div class="content-con-div">
-            <div class="content-con">
-                <img src="img/events.jpg" alt="" srcset="">
-                <div class="content-detail-event">
-                    <h4>Product Name</h4>
-                    <p>30% Off</p>
-                </div>
-                <div class="content-expired-event">
-                    Expired Date: 11/22/2020
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
+        error_reporting(0);
+        ini_set('display_errors', 0);
+
+        include('partial/header.php');
+
+        $link = $_GET['link'];
+        if($link == "anouncement"){
+            include_once('partial/anouncement.php');
+        }else if($link == "about"){
+            
+        }else if($link == "contact"){
+            
+        }else{
+            include_once('partial/home.php');
+        }
+
         include_once('partial/footer.php');
     ?>
     <script src="js/script.js"></script>
---
+++ partial/anouncement.php
@@ -0,0 +1,159 @@
+<?php
+ 
+    if(isset($_POST["promotion"])){
+        checkElement("promotion");
+    }else if(isset($_POST["discount"])){
+        checkElement("discount");
+    }else if(isset($_POST["event"])){
+        checkElement("event");
+    }else{
+        display();
+    }
+    function display(){
+        echo
+        '<div class="content-container">
+            <div class="content-title">
+                <h4>Promotions</h4>
+                <form class="content-title-div" method="post">
+                    <button type="submit" name="promotion">See More</button>
+                </form>
+            </div>
+            <div class="content-con-div">
+                <div class="content-con">
+                    <img src="img/promotion.jpg" alt="" srcset="">
+                    <div class="content-detail">
+                        <p>Product Name</p>
+                        <p>30% Off</p>
+                    </div>
+                    <div class="content-expired">
+                        Expired Date: 11/22/2020
+                    </div>
+                </div>
+                <div class="content-con">
+                    <img src="img/promotion.jpg" alt="" srcset="">
+                    <div class="content-detail">
+                        <p>Product Name</p>
+                        <p>30% Off</p>
+                    </div>
+                    <div class="content-expired">
+                        Expired Date: 11/22/2020
+                    </div>
+                </div>
+                <div class="content-con">
+                    <img src="img/promotion.jpg" alt="" srcset="">
+                    <div class="content-detail">
+                        <p>Product Name</p>
+                        <p>30% Off</p>
+                    </div>
+                    <div class="content-expired">
+                        Expired Date: 11/22/2020
+                    </div>
+                </div>
+                <div class="content-con">
+                    <img src="img/promotion.jpg" alt="" srcset="">
+                    <div class="content-detail">
+                        <p>Product Name</p>
+                        <p>30% Off</p>
+                    </div>
+                    <div class="content-expired">
+                        Expired Date: 11/22/2020
+                    </div>
+                </div>
+            </div>
+            <div class="content-title">
+                <h4>Special Discount</h4>
+                <form class="content-title-div" method="post">
+                    <button type="submit" name="discount">See More</button>
+                </form>
+            </div>
+            <div class="content-con-div">
+                <div class="content-con">
+                    <img src="img/special-discount.jpg" alt="" srcset="">
+                    <div class="content-detail">
+                        <p>Product Name</p>
+                        <p>30% Off</p>
+                    </div>
+                    <div class="content-expired">
+                        5:00PM to 7:00PM
+                    </div>
+                </div>
+            </div>
+            <div class="content-title">
+                <h4>New Events</h4>
+                <form class="content-title-div" method="post">
+                    <button type="submit" name="event">See More</button>
+                </form>
+            </div>
+            <div class="content-con-div">
+                <div class="content-con">
+                    <img src="img/events.jpg" alt="" srcset="">
+                    <div class="content-detail-event">
+                        <h4>Title</h4>
+                        <p>sdfds d sdffs sd sdds sdsdf ds sd sd fs  dsffdaf df faf dss afs fsa</p>
+                    </div>
+                    <div class="content-expired">
+                        Expired Date: 11/22/2020
+                    </div>
+                </div>
+            </div>
+        </div>';
+    }
+    function checkElement($element){
+        switch($element){
+            case "promotion":
+                echo 
+                '<div class="content-container">
+                    <h2 class="element-title">Promotion</h2>
+                    <div class="content-con-div">
+                        <div class="content-con">
+                            <img src="img/promotion.jpg" alt="" srcset="">
+                            <div class="content-detail">
+                                <p>Product Name</p>
+                                <p>30% Off</p>
+                            </div>
+                            <div class="content-expired">
+                                Expired Date: 11/22/2020
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                break;
+            case "discount":
+                echo 
+                '<div class="content-container">
+                    <h2 class="element-title">Special Discount</h2>
+                    <div class="content-con-div">
+                        <div class="content-con">
+                            <img src="img/special-discount.jpg" alt="" srcset="">
+                            <div class="content-detail">
+                                <p>Product Name</p>
+                                <p>30% Off</p>
+                            </div>
+                            <div class="content-expired">
+                                5:00PM to 7:00PM
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                break;
+            case "event":
+                echo 
+                '<div class="content-container">
+                    <h2 class="element-title">Happening Events</h2>
+                    <div class="content-con-div">
+                        <div class="content-con">
+                            <img src="img/events.jpg" alt="" srcset="">
+                            <div class="content-detail-event">
+                                <h4>Title</h4>
+                                <p>sdfds d sdffs sd sdds sdsdf ds sd sd fs  dsffdaf df faf dss afs fsa</p>
+                            </div>
+                            <div class="content-expired">
+                                Expired Date: 11/22/2020
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                break;
+        }
+    }
+?>
